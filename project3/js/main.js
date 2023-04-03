9f3b46c6f5960bb0fe9e98018feaf3ca94777d5a
Omar Grant <omar.e.grant@gmail.com>
2023-04-03 18:49:57 -0400

    started working on the LabelsAndButtons function

--- project3/js/main.js
+++ project3/js/main.js
@@ -71,10 +71,56 @@ function setupGame(){
     gameOverScene = new PIXI.Container();
     gameOverScene.visible = false;
     stage.addChild(gameOverScene);
+
+    createLabelsAndButtons();
 }
 
 function createLabelsAndButtons(){
+    let buttonStyle = new PIXI.TextStyle({
+        fill: 0xFF0000,
+        fontSize: 48,
+        fontFamily: "Futura"
+    });
 
+    // 1 - set up 'startScene'
+    // 1A - make top start label
+    let startLabel1 = new PIXI.Text("Circle Blast!");
+    startLabel1.style = new PIXI.TextStyle({
+        fill: 0xFFFFFF,
+        fontSize: 96,
+        fontFamily: "Futura",
+        stroke: 0xFF0000,
+        strokeThickness: 6
+    });
+    startLabel1.x = 50;
+    startLabel1.y = 120;
+    startScene.addChild(startLabel1);
+
+    // 1B - make middle start label
+    let startLabel2 = new PIXI.Text("R U Worthy..?");
+    startLabel2.style = new PIXI.TextStyle({
+        fill: 0xFFFFFF,
+        fontSize: 32,
+        fontFamily: "Futura",
+        fontStyle: "italic",
+        stroke: 0xFF0000,
+        strokeThickness: 6
+    });
+    startLabel2.x = 185;
+    startLabel2.y = 300;
+    startScene.addChild(startLabel2);
+
+    // 1C - make start game button
+    let startButton = new PIXI.Text("Enter, ... if you dare!");
+    startButton.style = buttonStyle;
+    startButton.x = 80;
+    startButton.y = sceneHeight - 100;
+    startButton.interactive = true;
+    startButton.buttonMode = true;
+    startButton.on("pointerup", startGame); // startGame is a function reference
+    startButton.on('pointerover', e => e.target.alpha = 0.7); // concise arrow function with no brackets
+    startButton.on('pointerout', e => e.currentTarget.alpha = 1.0); // ditto
+    startScene.addChild(startButton);
 }
 
 function startGame(){
